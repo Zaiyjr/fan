7e4a31847baf394dc8933798f52a1573c419239c
Zaiyjr <138764200+Zaiyjr@users.noreply.github.com>
2025-05-22 17:00:58 +0700

    Update Scene3D.js

--- src/components/Scene3D.js
+++ src/components/Scene3D.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls, Sphere, Text } from '@react-three/drei';
-import * as THREE from 'three';
+
 
 function FloatingHeart({ position, color, onClick }) {
   const mesh = useRef();
